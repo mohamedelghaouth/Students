6cd9f596ca67f658f5784210685cec1f89d341ef
mohamedelghaouth <44144437+mohamedelghaouth@users.noreply.github.com>
2019-11-23 20:04:34 +0000

    med

---
+++ students_bll/src/main/java/com/app/dao/StudentDao.java
@@ -0,0 +1,8 @@
+package com.app.dao;
+
+import com.bo.Student;
+import com.genericdao.api.GenericDao;
+
+public interface StudentDao extends GenericDao<Student, Long> {
+
+}
---
+++ students_bll/src/main/java/com/bo/Student.java
@@ -0,0 +1,65 @@
+package com.bo;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String nom;
+
+	private String prenom;
+
+	private Date dateNaissance;
+
+	private String email;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public Date getDateNaissance() {
+		return dateNaissance;
+	}
+
+	public void setDateNaissance(Date dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
